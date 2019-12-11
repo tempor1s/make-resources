0a14cb8a398d02e8d4c2416706879715d6ea66ec
Ben <benlaugherty@gmail.com>
2019-12-10 22:48:32 -0800

    [add] basic compose tweet view, url and template

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from tweets import views
+from tweets.views import ComposeTweet
 
 urlpatterns = [
     path('', views.index, name='homepage'),
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('tweet/compose/', ComposeTweet.as_view(), name='compose')
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from tweets.models import Tweet, Reply
+from users.models import Follower, Profile
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Count
+
 
 # Create your views here.
+# TODO: Turn all of these into Modals
 def index(request):
     return render(request, 'tweets/index.html', context={})
 
+
 def home(request):
     return render(request, 'tweets/home.html', context={})
+
+
+class ComposeTweet(LoginRequiredMixin, CreateView):
+    model = Tweet
+    fields = ['content']
+    template_name = 'tweets/compose_tweet.html'
+    success_url = '/home'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['header'] = 'Compose a new tweet'
+        return data
