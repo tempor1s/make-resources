d3fc108dead262b6f3f0213abdadf4afad069291
Ben <benlaugherty@gmail.com>
2019-12-10 21:37:34 -0800

    [add] new db name + migrations to maybe fix heroku version

--- make_resources/settings.py
+++ make_resources/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'make_resources.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database.sqlite3'),
     }
 }
 
