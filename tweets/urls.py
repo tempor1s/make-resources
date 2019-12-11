c525cda16ad3d07d088d767aa09e0758ff8dce8f
Ben <benlaugherty@gmail.com>
2019-12-10 22:58:48 -0800

    [add] delete view, url, tempalte

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from tweets import views
-from tweets.views import ComposeTweet
+from tweets.views import ComposeTweet, DeleteTweet
 
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('home/', views.home, name='home'),
-    path('tweet/compose/', ComposeTweet.as_view(), name='compose')
+    path('tweet/compose/', ComposeTweet.as_view(), name='compose-tweet'),
+    path('tweet/<int:pk>/delete/', DeleteTweet.as_view(), name='delete-tweet')
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -3,9 +3,12 @@ from tweets.models import Tweet, Reply
 from users.models import Follower, Profile
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.db.models import Count
 
+# helper func to make sure that the post's user is the logged in user, so that people can not delete/edit other users tweet
+def is_logged_user(post_user, logged_user):
+    return post_user == logged_user
 
 # Create your views here.
 # TODO: Turn all of these into Modals
@@ -31,3 +34,13 @@ class ComposeTweet(LoginRequiredMixin, CreateView):
         data = super().get_context_data(**kwargs)
         data['header'] = 'Compose a new tweet'
         return data
+
+
+class DeleteTweet(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_tweet.html'
+    success_url = '/home'
+    context_object_name = 'tweet'
+
+    def test_func(self, **kwargs):
+        return is_logged_user(self.get_object().author, self.request.user)
