4779df122e5cc94723318c42a17ef92b460c481c
Ben <benlaugherty@gmail.com>
2019-12-10 21:31:04 -0800

    [add] basic login, logout, register, homepage, index templates/views/urls

--- make_resources/urls.py
+++ make_resources/urls.py
@@ -27,7 +27,11 @@ urlpatterns = [
     # Admin Site
     path('admin/', admin.site.urls),
     # Use Tweet Application for / route.
-    path('', include('tweets.urls'))
+    path('', include('tweets.urls')),
+    # User Auth Views
+    path('register/', user_views.RegisterView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
 
 # for static files in debug mode
--- tweets/urls.py
+++ tweets/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from tweets import views
 
 urlpatterns = [
-    path('', views.index, name='homepage')
+    path('', views.index, name='homepage'),
+    path('home/', views.home, name='home')
 ]
